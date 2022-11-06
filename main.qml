43562f3b742ad04c5c5b7e65b98bab8019125411
DESKTOP-2FHRKCS\daisuke <d.morikawa.b@gmail.com>
2022-11-06 20:14:11 +0900

    OpenGL描画の最小サンプル

--- main.qml
+++ main.qml
@@ -1,9 +1,10 @@
 import QtQuick 2.3
+import QtQuick.Controls 2.3
 import Renderer 1.0
 
 Item {
-    width: 500
-    height: 600
+    width: 1920
+    height: 1080
 
     FBORenderer{
         id: fboRenderItem
@@ -11,10 +12,29 @@ Item {
     }
 
     Text {
+        id: text1
         anchors.bottom: fboRenderItem.bottom
         x: 20
         wrapMode: Text.WordWrap
 
         text: "This is an openGl rendering with QQuickFrameBufferObject"
+    }
+
+    Button {
+        x: 100
+        y: 100
+        text: "Button"
+    }
+
+    Timer {
+        id: timer1
+        interval: 1000
+        repeat: true
+        running: true
+        triggeredOnStart: true
+        onTriggered: {
+            text1.text = Date()
+            fboRenderItem.update()
+        }
     }
 }
--- opengl.py
+++ opengl.py
@@ -1,5 +1,6 @@
 from OpenGL.GL import *
-from PySide2.QtCore import QUrl, qDebug, QObject
+from PySide2.QtCore import QUrl, qDebug, QObject, QElapsedTimer
+from PySide2 import QtCore, QtGui
 from PySide2.QtGui import QGuiApplication, QOpenGLFramebufferObjectFormat, QOpenGLFramebufferObject
 from PySide2.QtQml import qmlRegisterType
 from PySide2.QtQuick import QQuickItem, QQuickView, QQuickFramebufferObject
@@ -9,20 +10,96 @@ class FbItemRenderer(QQuickFramebufferObject.Renderer):
     def __init__(self, parent=None):
         super(FbItemRenderer, self).__init__()
         qDebug("Creating FbItemRenderer")
+        self._window = None
+        self._program = None
+        self._profile = QtGui.QOpenGLVersionProfile()
+        self._profile.setVersion(3, 3)
+        self.gl = None
+        self._y = 0.5
+        self._timer = QElapsedTimer()
+        self._timer.start()
 
     def createFrameBufferObject(self, size):
         qDebug("Creating FrameBufferObject")
         format = QOpenGLFramebufferObjectFormat()
-        format.setAttachment(QOpenGLFramebufferObject.Depth)
+        format.setSamples(4)
         return QOpenGLFramebufferObject(size, format)
 
     def synchronize(self, item):
         qDebug("Synchronizing")
+        nsec = self._timer.nsecsElapsed()
+        self._timer.restart()
+        if nsec > 0:
+            fps = 1.0E+9 / nsec
+            qDebug("FPS " + str(round(fps, 3)))
+
+        super().synchronize(item)
+        self._y += 0.01
 
     def render(self):
         qDebug("Render")
         # Called with the FBO bound and the viewport set.
         # ... Issue OpenGL commands.
+        if self._program is None:
+            self._my_initialize()
+
+        # self.gl.glClearColor(0.8, 0.8, 0.8, 1)
+        # self.gl.glClear(GL_COLOR_BUFFER_BIT)
+        glClearColor(0.8, 0.8, 0.8, 1)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        values = [
+            0.5, -0.5,
+            -0.5, -0.5,
+            0.0, self._y,
+        ]
+
+        self._program.bind()
+        self._program.setUniformValue("myColor", QtGui.QVector3D(0, 1, 0))
+        self._program.enableAttributeArray(0)
+        self._program.setAttributeArray(0, values, 2)
+
+        # self.gl.glDrawArrays(GL_TRIANGLES, 0, 3)
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 3)
+
+        self._program.disableAttributeArray(0)
+        self._program.release()
+
+        # Not sure this is necessary
+        # self._window.resetOpenGLState()
+
+    def _my_initialize(self):
+        qDebug("_my_initialize")
+        if self._window is None:
+            return
+
+        self.gl = self._window.openglContext().versionFunctions(self._profile)
+        self.gl = QtGui.QOpenGLFunctions(QtGui.QOpenGLContext.currentContext())
+        self._program = QtGui.QOpenGLShaderProgram()
+        self._program.addCacheableShaderFromSourceCode(
+            QtGui.QOpenGLShader.Vertex,
+            """
+            # version 330 core
+            layout (location = 0) in vec2 vertices;
+            void main() {
+                gl_Position = vec4(vertices, 0.0, 1.0);
+            }
+            """)
+        self._program.addCacheableShaderFromSourceCode(
+            QtGui.QOpenGLShader.Fragment,
+            """
+            #version 330 core
+            uniform vec3 myColor;
+            void main() {
+                gl_FragColor = vec4(myColor,1.0);
+            }
+            """)
+        self._program.bindAttributeLocation( "vertices", 0 )
+        self._program.link()
+
+    def set_window(self, window):
+        qDebug("set_window")
+        self._window = window
 
 
 class FBORenderItem(QQuickFramebufferObject):
@@ -34,6 +111,7 @@ class FBORenderItem(QQuickFramebufferObject):
     def createRenderer(self):
         qDebug("Create renderer")
         self._renderer = FbItemRenderer()
+        self._renderer.set_window(self.window())
         return self._renderer
 
 
